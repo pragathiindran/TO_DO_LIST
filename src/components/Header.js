47b3a4a7e11a46891a696f04b2d86b9722023c6f
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2021-02-04 07:51:33 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h3>To-Do List</h3>
+      <p><center>Add to-do tasks to the list and once completed it can be deleted from the list. Very useful inorder to complete the task in priority or submission wise. The tasks can be edited as well.</center></p><br/><br/>
+    </div>
+  )
+}
+
+export default Header
